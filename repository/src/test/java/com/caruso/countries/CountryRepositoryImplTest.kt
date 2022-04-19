5e717ac12e7bbb5b33e4b921d8686d0d7a7e6acd
caruspi <pierluigi.caruso@pirelli.com>
2022-04-20 00:43:09 +0200

    add tests to CountryRepository

--- repository/src/test/java/com/caruso/countries/CountryRepositoryImplTest.kt
+++ repository/src/test/java/com/caruso/countries/CountryRepositoryImplTest.kt
@@ -1,6 +1,13 @@
 package com.caruso.countries
 
-import com.caruso.countries.repository.CountryRepositoryImpl
+import com.caruso.countries.repository.*
+import com.caruso.countries.repository.remote.CountryDto
+import com.caruso.countries.repository.remote.FlagsDto
+import com.caruso.countries.repository.remote.NameDto
+import com.caruso.countries.repository.remote.RemoteDataSource
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
@@ -8,11 +15,36 @@ import org.junit.Test
 
 class CountryRepositoryImplTest {
 
-    private val sut = CountryRepositoryImpl()
+    private val remote: RemoteDataSource = mockk {
+        coEvery { getAllCountries() } returns listOf<CountryDto>().success()
+    }
+    private val sut = CountryRepositoryImpl(remote)
+
+    private val countryDto = CountryDto(
+        cca3 = "ITA",
+        name = NameDto(commonName = "ITALY"),
+        flags = FlagsDto("png", "svg")
+    )
 
     @Test
     fun `should emit an empty country list`() = runBlocking {
-        val firstItem = sut.observeCountries().first()
-        assertTrue(firstItem.isEmpty())
+        val actual = sut.observeCountries().first().getOrNull()
+        assertTrue(actual!!.isEmpty())
+    }
+
+    @Test
+    fun `should emit a list with one Country`() = runBlocking {
+        coEvery { remote.getAllCountries() } returns listOf(countryDto).success()
+        val actual = sut.observeCountries().first().getOrNull()
+        val expected = listOf(Country("ITA", "ITALY"))
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should emit NetworkUnavailable on Network error`() = runBlocking {
+        coEvery { remote.getAllCountries() } returns NetworkUnavailable.error()
+        val actual = sut.observeCountries().first()
+        val expected = NetworkUnavailable.error()
+        assertEquals(expected, actual)
     }
 }
