8518a5358dcaee2410fe800909246ca2d7e545e1
caruspi <pierluigi.caruso@pirelli.com>
2022-04-21 09:45:12 +0200

    add should emit countries view model test

---
+++ core-test/src/main/java/com/caruso/countries/core_test/CoroutineRule.kt
@@ -0,0 +1,29 @@
+package com.caruso.countries.core_test
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CoroutineRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+
+    fun runBlockingTest(block: suspend (TestScope) -> Unit) =
+        runTest(testDispatcher) {
+            block(this)
+        }
+}
+
--- list/src/test/java/com/caruso/countries/list/CountryListViewModelTest.kt
+++ list/src/test/java/com/caruso/countries/list/CountryListViewModelTest.kt
@@ -1,10 +1,43 @@
 package com.caruso.countries.list
 
+import app.cash.turbine.test
+import com.caruso.countries.core_test.CoroutineRule
+import com.caruso.countries.repository.Country
+import com.caruso.countries.repository.CountryRepository
+import com.caruso.countries.repository.success
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class CountryListViewModelTest {
 
+    @get:Rule
+    val coroutineRule = CoroutineRule()
+
+    private val countries = listOf(
+        Country(id = "FRA", name = "FRANCE"),
+        Country(id = "ITA", name = "ITALY")
+    )
+
+    private val countryRepository: CountryRepository = mockk {
+        every { observeCountries() } returns flow { emit(countries.success()) }
+    }
+
     @Test
-    fun `should emit a state with one Country`() {
+    fun `should emit a state with two Countries`() = coroutineRule.runBlockingTest {
+        val sut = CountryListViewModel(countryRepository)
+        val expectedCountries = listOf(
+            Country(id = "FRA", name = "FRANCE"),
+            Country(id = "ITA", name = "ITALY")
+        )
+        val expected = CountryListState(expectedCountries)
+        sut.state.test {
+            assertEquals(expected, awaitItem())
+        }
     }
 }
