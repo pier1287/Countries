774ecf462e77d3bae9e021435b616b2321a77f5e
Caruso <pierluigi.caruso@prima.it>
2023-07-20 12:59:38 +0200

    add lint action

--- .github/workflows/lint_report.js
+++ .github/workflows/lint_report.js
@@ -1,11 +1,11 @@
 module.exports = async ({github, context, glob}) => {
-    const globber = await glob.create('**/*.xml')
+    const globber = await glob.create('**/reports/*.xml')
     const files = await globber.glob()
 
     await github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: `⚠️ Ciao ho trovato ${files.length} files .xml`
+        body: '⚠️ Hi i found these xml files in the reports folder: \n files.join' + files.join('\n')
     })
 }
