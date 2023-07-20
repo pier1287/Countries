d2f92226096f6558dbbe351d149cfaeff9fc1a77
Caruso <pierluigi.caruso@prima.it>
2023-07-20 12:03:54 +0200

    add lint action

--- .github/workflows/lint_report.js
+++ .github/workflows/lint_report.js
@@ -1,9 +1,9 @@
-module.exports = async ({github, context, xmlFiles}) => {
+module.exports = async ({github, context}) => {
 
     await github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: '⚠️ ${xmlFiles.length} xml files found'
+        body: '⚠️ Ciao Ciccio'
     })
 }
